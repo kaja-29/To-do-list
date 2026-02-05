470f60bb92781aa63b18a3b5c2e90f904ef8409f
kaja <mr.kajamaideen@gmail.com>
2026-02-05 15:02:49 +0530

    delete button & property added

--- main.js
+++ main.js
@@ -1,35 +1,55 @@
 const input = document.getElementById("input");
 const addbtn = document.getElementById("addbtn");
 const todocontent = document.getElementById("todolist");
+const todocontentchild = document.getElementById("todolistchild");
+const del = document.getElementById("del");
 
 let todos = [];
 
+
+
 addbtn.addEventListener("click",()=>{
-    todos.push(input.value);''
-    add(input.value);
+    const value = input.value.trim();
+    if(value === ""){
+        alert("Pls enter your Task");
+        return;
+    }
+    todos.push(value);
+    add(value);
     input.value = "";
+    console.log(todos);
 })
 
 function add(togo){
+    const rows = document.createElement("div");
+    rows.className = "todolistchild";
+
     const para = document.createElement("p");
     para.innerText = togo;
-    todocontent.appendChild(para);
+
+    const delbtn = document.createElement("button");
+    delbtn.innerText = "Delete-task";
+
+    todocontent.appendChild(rows);
+    rows.appendChild(para);
+    rows.appendChild(delbtn);
     
     para.addEventListener("click",()=>{
         para.style.textDecoration = "line-through";
-        remove(togo);
     })
 
-    para.addEventListener("dblclick",()=>{
-        todocontent.removeChild(para);
-        remove(togo);    
+    delbtn.addEventListener("click",()=>{
+        todocontent.removeChild(rows);
+        remove(togo);
     })
 }
 
 function remove(togo){
     const index = todos.indexOf(togo);
-    if(index>-1){
-        todos.splice(index,1);
+        if(index>-1){
+            todos.splice(index,1);        
     }
-
 }
+
+
+
